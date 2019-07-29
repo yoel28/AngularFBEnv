0a9d6ebdee06dd26ad58c9a818b7a486bbde777e
Yoel Rodriguez <yo3l.m18@gmail.com>
2019-07-29 16:54:28 -0500

    add app phone login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-test';
-}
+export class AppComponent {}
